c24a400cdb9f35c44bfa84079aa6de853939f908
Khizer Jan <khizer.jaan7@gmail.com>
2022-05-16 02:33:45 +0500

    Updates to get environment variable

--- back-end/routes/hello_world.js
+++ back-end/routes/hello_world.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("Hello World!");
+    var name   = process.env.Name;
+    res.send("Hello " + name);
 });
 
 module.exports = router;
